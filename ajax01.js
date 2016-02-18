ebfa566650465c695806ebb41c04d863b5b7246c
fubei cheng <relaxto@qq.com>
2016-02-18 23:29:51 +0800

    adddata

--- ajax01.js
+++ ajax01.js
@@ -1,8 +1,8 @@
 /*
 * @Author: chengfubei
 * @Date:   2016-01-05 13:36:12
-* @Last Modified by:   chengfubei
-* @Last Modified time: 2016-01-05 17:24:37
+* @Last Modified by:   fubei cheng
+* @Last Modified time: 2016-02-18 23:17:26
 */
 
 'use strict';
@@ -23,9 +23,11 @@ aj.send();
 //4.listen
 aj.onreadystatechange = function(){
     if(aj.readyState ==4){
+      //成功状态执行
       if(aj.status ==200){
         success(aj.responseText)
       }
+      //失败状态执行
       else{
         if(faild)
         faild(aj.status)
